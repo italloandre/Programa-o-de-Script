a2d31f77421bd4bd1f972ceec00920f3d0668a7d
italloandre <39264295+italloandre@users.noreply.github.com>
2021-11-09 23:48:20 -0300

    Update Q 2.sh

--- Q 2.sh
+++ Q 2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "qual o nome dos 3 diretorios que você deseja listar?" dir1 dir2 dir3 dir4
+read -p "qual o nome dos 4 diretorios que você deseja listar?" dir1 dir2 dir3 dir4
 
 echo "lista do diretorio 1:" $(ls $dir1) > /tmp/out.txt
 echo "lista do diretorio 2:" $(ls $dir2) > /tmp/out.txt
